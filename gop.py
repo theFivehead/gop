e69192cd9e9db74996dcf36960fe912a76f3294e
theFivehead <marek.rohac5@gmail.com>
2025-03-16 17:15:22 +0100

    Update gop.py
    odstranění AKS

--- gop.py
+++ gop.py
@@ -1,8 +1,8 @@
 import time
 import itertools
 import timeit
+import math
 from multiprocessing import Process, cpu_count
-from idlelib.configdialog import is_int
 
 #definovani promennych --------------------------------------------------------------------------
 soubor_prvocislo="prvocisla.txt"
@@ -25,19 +25,47 @@ def ulozeni_prvocisel_soubor(prvocisla,soubor_cesta="ulozena_prvocisla"):
         soubor.write(str(prvocislo)+"\n")
     soubor.close()
 
-def erastenovoSito():
+def atkinovoSito():
     pass
 
-def millerRabin():
+def millerRabin(n):
+    a=()
+    if n < 2047:
+        a = (2,)
+    elif n < 1373653:
+        a = (2, 3)
+    elif n < 9080191:
+        a = (31, 73)
+    elif n < 25326001:
+        a = (2, 3, 5)
+    elif n < 3215031751:
+        a = (2, 3, 5, 7)
+    elif n < 4759123141:
+        a = (2, 7, 61)
+    elif n < 1122004669633:
+        a = (2, 13, 23, 1662803)
+    elif n < 2152302898747:
+        a = (2, 3, 5, 7, 11)
+    elif n < 3474749660383:
+        a = (2, 3, 5, 7, 11, 13)
+    elif n < 341550071728321:
+        a = (2, 3, 5, 7, 11, 13, 17)
+    elif n < 3317044064679887385961981:
+        a = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41)
+    d=int((n-1)/2)
+    for i in range(len(a)):
+        p=pow(a[0],d,n)
+        if p!=1 and p-n!=-1:
+            return False
+    return True
+def postupneDeleni():
     pass
 
-def AKS():
-    pass
 
 
 
 print("chcete otestova prvocislo")
 
 test=nacteni_cisel_soubor(soubor_prvocislo)
-print(test)
+#print(test)
 ulozeni_prvocisel_soubor(test)
