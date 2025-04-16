d9c76bc172019776042c46f9c5b02babd61b58b7
marek <marek.rohac5@gmail.com>
2025-04-16 21:10:07 +0200

    kod dokončen. Teď otestovat rychlosti

--- gop.py
+++ gop.py
@@ -1,3 +1,5 @@
+import datetime
+from contextlib import nullcontext
 from random import random, randrange
 
 import time
@@ -5,7 +7,6 @@ import itertools
 import timeit
 import math
 import sys
-from curses.ascii import isblank
 from multiprocessing import Process, cpu_count
 
 #definovani promennych --------------------------------------------------------------------------
@@ -24,6 +25,14 @@ generovat_prv=False     #prepinac -g
 ulozit_do_souboru=False #prepinac -u
 nacist_ze_souboru=""    #prepinac -n="cesta ke souboru"
 
+#otevre soubor pro debugerr
+debug = open("udalosti.log","a")
+
+def zapis_debug(text):
+    stext=str(text)
+    debug.write(time.strftime("%H-%M-%S--%d/%m/%Y")+":"+stext+"\n")
+
+
 def help():
     print("gop.py [cislo algoritmu] 1-atkinovoSito 2-millerRabin 3-postupneDeleni"
           "\n-g pro generování prvočísel\n-u ulozit_do_souboru\n-n=""cesta ke souboru""\n--help zobrazí tento text")
@@ -31,6 +40,7 @@ def help():
 
 #funkce ktera nacte cisla ze souboru a vráti pole s int
 def nacteni_cisel_soubor(nacteni_lokace):
+    zapis_debug("nacitam cisla ze souboru:"+nacteni_lokace)
     soubor=open(nacteni_lokace, "r")
     nactena_data=soubor.read().split("\n")
     cisla=[0]
@@ -38,6 +48,7 @@ def nacteni_cisel_soubor(nacteni_lokace):
         if potencialni_cislo.isdigit():
             cisla.append(int(potencialni_cislo))
     soubor.close()
+    zapis_debug("nactena cisla:"+str(cisla))
     return cisla
 
 def ulozeni_prvocisel_soubor(prvocisla,soubor_cesta="ulozena_prvocisla.txt"):
@@ -45,18 +56,29 @@ def ulozeni_prvocisel_soubor(prvocisla,soubor_cesta="ulozena_prvocisla.txt"):
     for prvocislo in prvocisla:
         soubor.write(str(prvocislo)+"\n")
     soubor.close()
+    zapis_debug("ukladam prvocisla:"+str(prvocisla)+"do souboru:"+soubor_cesta)
 
 def atkinovoSito(strop):
     # Vypsání známých prvočísel 2 a 3, pokud to limit dovoluje
-    result = []
+    p = []
+    if strop<1:
+        return p
+    elif strop > sys.maxsize:
+        print("hodnota prvocisla je vetsi nez je mozna pamet\nzkuste jiny algoritmus\n")
+        return p
+
     if strop > 2:
-        result.append(2)
+        p.append(2)
     if strop > 3:
-        result.append(3)
+        p.append(3)
 
     # Inicializace pole pro označení čísel, zda jsou kandidáty na prvočísla
     # Používáme seznam o velikosti limit+1, abychom mohli indexovat až do limit
-    sieve = [False] * (strop + 1)
+    try:
+        sieve = [False] * (strop + 1)
+    except MemoryError:
+        print("prvocislo nebylo mozno nacist do pameti")
+        return p
 
     # Sběr pomocí podmínek dle Atkinova algoritmu
     for x in range(1, int(math.sqrt(strop)) + 1):
@@ -83,11 +105,14 @@ def atkinovoSito(strop):
     # Sběr prvočísel z vyznačeného pole
     for a in range(5, strop):
         if sieve[a]:
-            result.append(a)
+            p.append(a)
+    print(p)
+    return p
 
-    return result
 
 def millerRabin(n):
+    if(n<1):
+        return False
     a=()
     if n < 2047:
         a = (2,)
@@ -120,16 +145,19 @@ def millerRabin(n):
             return False
     return True
 def postupneDeleni(n):
+    if(n<1):
+        return False
     hranice=int(math.sqrt(n))+1
     for i in range(2,int(hranice),1):
-        print(i)
         if n%i==0:
             return False
     return True
 
 #nacteni hodnot z prikazove radky --------------------------------------------------------------------------
+
 if len(sys.argv) <= 1:
     print("zadne hodnoty nebyly zadany")
+    zapis_debug("zadne hodnoty nebyly zadany")
 else:
     if sys.argv[1].isdigit():
         cislo_algoritmu=int(sys.argv[1])
@@ -140,49 +168,74 @@ else:
                 if sys.argv[i].find("--help") != -1:
                     help()
                 if sys.argv[i].find("u") != -1:
-                    print("prvocisla se ulozi do souboru")
+                    zapis_debug("prvocisla se ulozi do souboru")
                     ulozit_do_souboru=True
                 if sys.argv[i].find("g")  != -1:
-                    print("prvocisla se budou generovat")
+                    zapis_debug("prvocisla se budou generovat")
                     generovat_prv=True
                 if sys.argv[i].find("n=")  != -1:
                     soubor_prvocislo_n=sys.argv[i][sys.argv[i].find("=")+1:]
-                    print("prvocisla se nactou ze souboru:"+soubor_prvocislo_n)
                     cisla=nacteni_cisel_soubor(soubor_prvocislo_n)
             elif sys.argv[i].isdigit():
                 cisla.append(int(sys.argv[i]))
     else:
         help()
 if not cisla:
-    print("suss")
     help()
 
-cas_Z=time.time()
+
+
+zacatek_hlaska=""
+if generovat_prv:
+    zacatek_hlaska+="generovani"
+else:
+    zacatek_hlaska += "overovani"
+zacatek_hlaska+=" s algoritmem "
+match cislo_algoritmu:
+    case 1:zacatek_hlaska+="atkinovo sito"
+    case 2:zacatek_hlaska+="miller rabin"
+    case 3:zacatek_hlaska+="postupne deleni"
+zapis_debug("spoustim "+zacatek_hlaska)
+cas_Z=time.perf_counter()
 if generovat_prv: #dodelat zde se budou generovat prvocisla do daneho cisla pomoci zvoleneho algoritmu
     match cislo_algoritmu:
         case 1:
-            atkinovoSito()
+            for cislo in cisla:
+                prvocisla = atkinovoSito(cislo)
         case 2:
             for cislo in cisla:
                 i=2
-                while i<=cislo:
+                while i<cislo:
                     if millerRabin(i):
                         prvocisla.append(i)
                     i+=1
         case 3:
-            postupneDeleni()
+            for cislo in cisla:
+                i = 2
+                while i < cislo:
+                    if postupneDeleni(i):
+                        prvocisla.append(i)
+                    i += 1
 else:       #a zde se pouze overi jestli se jedna o prvocisla zase pomoci zvoleneho algoritmu
     match cislo_algoritmu:
         case 1:
-            atkinovoSito()
+            for cislo in cisla:
+                seznam = atkinovoSito(cislo+1)
+                if cislo == seznam[len(seznam)-1]:
+                    prvocisla.append(cislo)
         case 2:
             for cislo in cisla:
                 if millerRabin(cislo):
                     prvocisla.append(cislo)
         case 3:
-            postupneDeleni()
-cas_K = time.time() - cas_Z
-print("generovani prvocisel trvalo:"+str(cas_K)+"s")
+            for cislo in cisla:
+                if postupneDeleni(cislo):
+                    prvocisla.append(cislo)
+cas_K = (time.perf_counter() - cas_Z) * 1_000_000
+
+gen_hlaska=f"{"generovani" if generovat_prv else "overovani"} trvalo:{cas_K:.2f}ms"
+print(gen_hlaska)
+zapis_debug(gen_hlaska)
 
 #ulozi nebo vytiskne na obrazovku dle toho co si uzivatel zvoli
 if ulozit_do_souboru:
@@ -192,15 +245,19 @@ else:
         print(prvocislo)
 if not prvocisla and not generovat_prv:
     print("žádné z čísel není prvočíslo")
-print(prvocisla)
 
 #ulozi cas genrovani prvocisel do souboru
 CIFRY=10
 with open("./prvocisla_cas.log", "a",encoding='utf-8') as time_log:
     operace= "generování" if generovat_prv else "ověřování"
     datum=time.strftime("%H-%M-%S--%d/%m/%Y")
-    prvocislo=max(prvocisla)
+    if not prvocisla:
+        prvocislo=-1
+    else:
+        prvocislo=max(prvocisla)
     if len(str(abs(int(prvocislo)))) >= CIFRY:
         time_log.write(f"datum: {datum} čas: {cas_K:.5f} prvočíslo: {prvocislo:.{CIFRY}e} operace: {operace} algoritmus: {algoritmus[cislo_algoritmu-1]}\n")
     else:
         time_log.write(f"datum: {datum} čas: {cas_K:.5f} prvočíslo: {prvocislo} operace: {operace} algoritmus: {algoritmus[cislo_algoritmu-1]}\n")
+
+debug.close()
